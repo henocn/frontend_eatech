a749dab491731220f14baa806595dafd878c2af4
Henoc N'GASAMA <ngasamah@gmail.com>
2026-01-14 10:23:56 +0000

    studio list cards and their design

--- src/components/studios/StudioList.jsx
+++ src/components/studios/StudioList.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import "./StudioList.css";
+import api from "../../utils/axiosInstance";
 
 const StudioList = () => {
-  return <div cla>StudioList</div>;
+  const [studios, setStudios] = useState([]);
+
+  useEffect(() => {
+    const fetchStudios = async () => {
+      try {
+        const studios = await api.get("/studios");
+        setStudios(studios);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+
+    fetchStudios();
+  }, []);
+
+  return (
+    <div className="studio-list">
+      {studios.map((studio) => (
+        <div className="studio-card" key={studio.id}>
+          <div className="studio-image">
+            <img src={studio.image} alt={studio.name} />
+          </div>
+
+          <div className="studio-content">
+            <h3 className="studio-title">{studio.name}</h3>
+            <p className="studio-location">{studio.location}</p>
+            <p className="studio-description">{studio.description}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default StudioList;
