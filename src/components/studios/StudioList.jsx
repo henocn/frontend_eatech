4a9c67f461a0accf2b403508ddf0a76c8da0a20b
Henoc N'GASAMA <ngasamah@gmail.com>
2026-01-14 09:57:45 +0000

    change the way to generalize api calling via axios

---
+++ src/components/studios/StudioList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./StudioList.css";
+
+const StudioList = () => {
+  return <div cla>StudioList</div>;
+};
+
+export default StudioList;
--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
+import StudioList from "../components/studios/StudioList";
+import "../App.css";
 
 const BookingPage = () => {
   return (
-    <div>
-      <div className="app">
-        <Header />
-        <Footer />
-      </div>
+    <div className="app">
+      <Header />
+      <main>
+        <StudioList />
+      </main>
+      <Footer />
     </div>
   );
 };
